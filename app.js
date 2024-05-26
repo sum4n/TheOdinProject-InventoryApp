fcd1f21694f811988fabb7d00e41f23d72fa23bb
Suman Das <sum4ndas@gmail.com>
2024-05-26 09:08:09 +0530

    Update index.js to redirect to '/catalog', update app.js to import catalog route and add catalog route to middleware chain.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+// Import routes for "catalog" area of site.
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
@@ -31,6 +33,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+// Add catalog routes to middleware chain.
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
