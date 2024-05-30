b502def9d97ddef7714e001d12adfc26a830bfbe
Suman Das <sum4ndas@gmail.com>
2024-05-30 09:13:51 +0530

    Create Item from -> update 'item_create_get()', 'item_create_post()' and create 'item_form.pug'

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,6 +4,7 @@ const Slot = require("../models/slot");
 const ItemInstance = require("../models/iteminstance");
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   // Get details of items, sellers, slots and item instances counts (in paraller)
@@ -67,13 +68,61 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 
 // Display Item create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create GET");
+  // Get all slots which can be used for adding to our book.
+  const allSlots = await Slot.find({}).exec();
+
+  res.render("item_form", {
+    title: "Create Item",
+    slots: allSlots,
+  });
 });
 
 // Handle Item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create POST");
-});
+exports.item_create_post = [
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty.")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("quality", "Quality must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("slot", "Slot must not be empty").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create an Item object with the escaped and trimmed data.
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      quality: req.body.quality,
+      slot: req.body.slot,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all slots for form.
+      const allSlots = await Slot.find({}).exec();
+
+      res.render("item_form", {
+        title: "Create Item",
+        slots: allSlots,
+        item: item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid. Save item.
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 
 // Display Item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
